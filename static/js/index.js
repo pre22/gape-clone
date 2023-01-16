c2df550e53c228cd1099dd1a88caabcfd6c0464a
Precious Chika <43546663+pre22@users.noreply.github.com>
2023-01-16 16:46:35 +0100

    static: s snippet

---
+++ static/js/index.js
@@ -0,0 +1,6 @@
+var carouselWidth = document.getElementsByClassName('.custom-carousel').scrollWidth;
+var firstParent = carouselWidth.Item(0);
+var cardWidth = firstParent.querySelectorAll('.carousel-item');
+
+var scrollPosition = 0;
+
