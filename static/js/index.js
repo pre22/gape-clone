c8a71fbc98c84de5c43e0c004b84507d15a21f20
Precious Chika <43546663+pre22@users.noreply.github.com>
2023-01-17 10:11:35 +0100

    static: cleared js file

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
-var carouselWidth = document.getElementsByClassName('.custom-carousel').scrollWidth;
-var firstParent = carouselWidth.Item(0);
-var cardWidth = firstParent.querySelectorAll('.carousel-item');
+// var carouselWidth = document.getElementsByClassName('.custom-carousel').scrollWidth;
+// var firstParent = carouselWidth.Item(0);
+// var cardWidth = firstParent.querySelectorAll('.carousel-item');
 
-var scrollPosition = 0;
+// var scrollPosition = 0;
 
